a0d96881714233756917305a40a615d76e3a0cc9
Matthew Herman <mattherman11@gmail.com>
2024-02-12 22:10:58 -0600

    Implemented tag searching and added logging

--- lua/nvim-munin/init.lua
+++ lua/nvim-munin/init.lua
@@ -1,5 +1,3 @@
--- autocmd on directory open? to sync repo
--- autocmd on file write to update repo
 -- command to search by text
 -- command to search by tag
 -- command to search by category
@@ -8,7 +6,8 @@
 -- picker to link to other note
 -- command to navigate link
 
-print(":lua/nvim-munin/init.lua")
+local log = require("nvim-munin.log")
+require("munin.logger").configure(log)
 
 local munin = require("munin.repo")
 local utils = require("munin.utils")
@@ -28,19 +27,21 @@ end
 
 local function init()
     local cwd = vim.loop.cwd()
-    print("cwd = "..cwd)
 
-    print("Initializing repo")
+    log.fmt_trace("Initializing repo: %s", cwd)
     local repo = munin.init(cwd)
 
     if repo.exists() then
-        print("Found repo at "..repo._config_path)
+        log.trace("Found repo at "..repo._config_path)
 
         if M.config.autoSync then
-            print("Auto sync enabled, indexing repo...")
-            local index_err = require("munin.indexer").index(repo --[[ TODO: pass patterns --]])
+            log.trace("Auto sync enabled, indexing repo...")
+            local index_err = require("munin.indexer").index(repo)
             if index_err then
-                print(string.format("Failed to index repo (%s): %s", repo._path, index_err))
+                log.fmt_error("Failed to index repo (%s): %s", repo._path, index_err)
+                vim.notify("Failed to sync munin database")
+            else
+                vim.notify("Munin database synced")
             end
         end
 
@@ -49,35 +50,35 @@ local function init()
 end
 
 local function buffer_entered(event)
-    print(string.format("event fired: %s", vim.inspect(event)))
+    log.fmt_trace("event fired: %s", vim.inspect(event))
     if M.repo then
         local parsed_path = utils.parse_file_path(event.file, M.repo._path)
         local note = M.repo.get_note(parsed_path.path)
         if note then
-            print(string.format("Opened note titled %s", note.title))
+            log.fmt_trace("Opened note titled %s", note.title)
         end
     end
 end
 
 local function buffer_write(event)
-    print(string.format("event fired: %s", vim.inspect(event)))
+    log.fmt_trace("event fired: %s", vim.inspect(event))
     if M.repo then
         local buffer_lines = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
         local content = table.concat(buffer_lines, "\n")
         local parsed_path = utils.parse_file_path(event.file, M.repo._path)
-        print(vim.inspect(parsed_path))
+        log.fmt_trace("Saving note buffer for file: %s", vim.inspect(parsed_path))
         local note, err = M.repo.save_note(parsed_path.title, content, parsed_path.category)
         if err then
-            vim.notify(err, vim.log.levels.ERROR)
+            log.error(err, vim.log.levels.ERROR)
         end
         if note then
-            print(string.format("Saved note titled %s", note.title))
+            log.fmt_trace("Saved note titled %s", note.title)
         end
     end
 end
 
 local function directory_changed(event)
-    print(string.format("event fired: %s", vim.inspect(event)))
+    log.fmt_trace("event fired: %s", vim.inspect(event))
     M.repo = init()
 end
 
@@ -104,6 +105,36 @@ function M.setup(config)
         pattern = M.config.patterns,
         callback = buffer_write
     })
+end
+
+local function find_tag_under_cursor(line, cursor_pos)
+    local first, last = 0, 0
+    while true do
+        first, last = line:find("@([^%s]+)", first+1)
+        if not first then break end
+        if cursor_pos >= first and cursor_pos <= last then
+            return line:sub(first + 1, last) -- Omit the '@'
+        end
+    end
+end
+
+function M.search_tag_under_cursor()
+    local r, c = unpack(vim.api.nvim_win_get_cursor(0))
+    local line = vim.api.nvim_buf_get_lines(0, r-1, r, false)[1]
+    local tag = find_tag_under_cursor(line, c + 1)
+    if not tag then
+        log.trace("No tag found")
+        return
+    end
+    log.trace("Searching tag: "..tag)
+    local notes, err = M.repo.get_notes_by_tag(tag)
+    if notes then
+        local message = string.format("Found %d notes matching tag", #notes)
+        log.trace(message)
+        vim.notify(message)
+    else
+        log.error(err)
+    end
 end
 
 return M
--- plugin/init.lua
+++ plugin/init.lua
@@ -1,2 +1,1 @@
-print(":plugin/init.lua")
 
